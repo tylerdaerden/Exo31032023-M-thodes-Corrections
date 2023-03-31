cbb2f85e740ef81f666af55b84596ed78285a96e
Tyler Daerden <daerdentyler@gmail.com>
2023-03-31 10:32:12 +0200

    Ajoutez des fichiers projet.

---
+++ Exo31032023/Program.cs
@@ -0,0 +1,39 @@
+﻿#region Exos Méthodes
+
+#region Exo 1.1 Celsius , Farhenheit
+
+//1.Dans les structures Celsius et Fahrenheit, écrire la fonction de conversion de l’une vers l’autre.
+
+
+
+
+
+
+
+#endregion
+
+#region Exo 1.2 , Equation second degré
+
+//2.Ecrire une structure pour résoudre une équation du second degré.
+//La structure devra contenir : 
+//Trois variables membres publiques A, B et C de type double.
+//Une méthode publique « Resoudre » retournant une valeur de type « bool » stipulant si une réponse a été trouvée et devra retourner également les
+//valeurs de X1 et de X2 de type double.
+//Si aucune solution n’a été trouvée, les valeurs de X1 et de X2 doivent être égale à « null ».
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endregion
+#endregion
+
+
